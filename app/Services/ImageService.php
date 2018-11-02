f28abc6098038a4019ec798f405724083853818c
Igor Moraes <igor.sgm@gmail.com>
2018-11-02 18:44:15 -0300

    + ImageService created to handle images, containing business logics

---
+++ app/Services/ImageService.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use Illuminate\Http\UploadedFile;
+use Request;
+use Symfony\Component\HttpFoundation\File\Exception\CannotWriteFileException;
+
+class ImageService
+{
+    /**
+     * @param Request $request
+     *
+     * @return string
+     * @throws FileNotFoundException
+     */
+    public function handleImage($request)
+    {
+        if ($request->hasFile('image_file')) {
+            return $this->saveImageFile($request->file('image_file'));
+        }
+
+        return $request->get('image_file');
+    }
+
+    /**
+     * Save image file inside storage folder and retrieve it's information
+     *
+     * @param UploadedFile $file
+     *
+     * @return string
+     * @throws CannotWriteFileException
+     * @throws FileNotFoundException
+     */
+    public function saveImageFile($file)
+    {
+        try {
+            $fileName = $file->getClientOriginalName();
+            $file->move(public_path('uploadedimages/'), $fileName);
+        } catch (\Exception $exception) {
+            throw new CannotWriteFileException('Image could not be saved', 422);
+        }
+
+        return $fileName;
+    }
+}
